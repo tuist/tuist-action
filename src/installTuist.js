082aa3799916794b72213bbbe9f05a31f9a54771
Pedro Piñera <pepibumur@gmail.com>
2021-03-22 11:19:47 +0100

    Fix

--- src/installTuist.js
+++ src/installTuist.js
@@ -6,7 +6,10 @@ const downloadFile = require("./downloadFile");
 module.exports = () => {
   const tmpobj = tmp.dirSync();
   const installScriptPath = path.join(tmpobj.name, "install.sh");
+  console.log("Downloading Tuist installer...");
   downloadFile("https://install.tuist.io", installScriptPath);
   child_process.execSync(`chmod +x ${installScriptPath}`);
+  console.log("Installing Tuist...");
   child_process.execSync(installScriptPath);
+  console.log("Tuist has been installed.");
 };
