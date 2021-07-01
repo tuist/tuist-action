c640d6322fda4240df99f358fe9520ddec60c763
Pedro Piñera <pedro@ppinera.es>
2021-07-01 18:49:25 +0200

    Pass headers

--- src/latestReleaseTuistEnvDownloadURL.js
+++ src/latestReleaseTuistEnvDownloadURL.js
@@ -1,8 +1,14 @@
 const axios = require("axios");
+const process = require("process");
 
 module.exports = async function () {
   const url = "https://api.github.com/repos/tuist/tuist/releases/latest";
-  const response = await axios.get(url);
+  const response = await axios.get(url, {
+    headers: {
+      Authorization: `token: ${process.env.GITHUB_TOKEN}`,
+      Accept: "application/vnd.github.v3+json",
+    },
+  });
   return response["assets"].find((asset) => {
     asset["name"] === "tuistenv.zip";
   })["browser_download_url"];
