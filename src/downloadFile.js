2e1cb555f9989f1cb2138831eba43d8403730730
Pedro Piñera <pedro@ppinera.es>
2021-12-14 10:37:46 +0100

    Change download logic

--- src/downloadFile.js
+++ src/downloadFile.js
@@ -1,36 +1,11 @@
-const https = require('https');
-const fs = require('fs');
-
-function downloadFileFollowRedirect(url, file, resolve, reject) {
-  https.get(url, (response) => {
-    if (response.statusCode === 301 || response.statusCode === 302) {
-      return downloadFileFollowRedirect(
-        response.headers.location,
-        file,
-        resolve,
-      );
-    }
-    if (response.statusCode >= 400) {
-      return reject(
-        new Error(
-          `Getting the file at ${url} failed with HTTP status code ${response.statusCode}`,
-        ),
-      );
-    }
-
-    response.pipe(file);
-    return file.on('finish', () => {
-      file.close(() => {
-        resolve();
-      });
-    });
-  });
-}
+const Downloader = require('nodejs-file-downloader');
+const path = require('path');
 
 module.exports = async (url, into) => {
-  const file = fs.createWriteStream(into);
-  const promise = new Promise((resolve, reject) => {
-    downloadFileFollowRedirect(url, file, resolve, reject);
+  const downloader = new Downloader({
+    url,
+    directory: path.dirname(into),
+    fileName: path.basename(into),
   });
-  await promise;
+  await downloader.download();
 };
